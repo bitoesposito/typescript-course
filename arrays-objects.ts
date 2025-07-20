1b36b5b8045bba37a739b4ff5e0d90245f869efa
Vito Esposito <info@vitoesposito.it>
2025-07-20 10:51:27 +0200

    s2e22

--- arrays-objects.ts
+++ arrays-objects.ts
@@ -1,4 +1,12 @@
-let hobbies: (string | number)[]
+// let hobbies: (string | number)[]
+let hobbies: Array< string | number >
 hobbies = ['Sports', 'Cooking'];
 
 hobbies.push(10);
+
+let possibleResults: [number, 20];
+
+possibleResults = [10, 20];
+// possibleResults = [11, 21];
+// possibleResults = [10, 20, 30];
+
