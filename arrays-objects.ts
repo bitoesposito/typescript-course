f2ef4dd8f7956515d26c59474a0e98c903c1571c
Vito Esposito <info@vitoesposito.it>
2025-07-20 10:58:18 +0200

    s2e23

--- arrays-objects.ts
+++ arrays-objects.ts
@@ -10,3 +10,20 @@ possibleResults = [10, 20];
 // possibleResults = [11, 21];
 // possibleResults = [10, 20, 30];
 
+let user: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role : {
+        description: string;
+        id: number;
+    }
+} = {
+    name: 'Max',
+    age: 30,
+    hobbies: ['Sports', 'Cooking'],
+    role: {
+        description: 'admin',
+        id: 1
+    }
+}
