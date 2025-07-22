a17f2c56c249c176a395ba60632c00cae8d78961
Vito Esposito <info@vitoesposito.it>
2025-07-22 21:35:54 +0200

    s17e223

---
+++ s17 - 3rd party libraries/app.ts
@@ -0,0 +1,5 @@
+import _ from "lodash";
+
+const number = [1,2,3,4,5,6,7,8,9,10]
+
+console.log(_.chunk(number, 2))
---
+++ s17 - 3rd party libraries/dist/app.js
@@ -0,0 +1,4 @@
+import _ from "lodash";
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(_.chunk(number, 2));
+//# sourceMappingURL=app.js.map
