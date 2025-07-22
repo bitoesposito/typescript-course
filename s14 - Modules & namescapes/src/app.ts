4247717e4b17b833164a5ba7cc7b78c427ea1ca3
Vito Esposito <info@vitoesposito.it>
2025-07-22 20:45:56 +0200

    s14e193

---
+++ s14 - Modules & namescapes/src/app.ts
@@ -0,0 +1,6 @@
+import { ProjectList } from './component/project-list';
+import { ProjectInput } from './component/project-input';
+
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
